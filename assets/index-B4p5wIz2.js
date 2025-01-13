43fba355795e32b4f14109e1c72ce4894b275bca
ck-js <ckentertainmentinfo@gmail.com>
2025-01-13 19:16:04 +0700

    Updates

--- assets/index-B4p5wIz2.js
+++ assets/index-B4p5wIz2.js
@@ -128,7 +128,7 @@ This project involves developing a Single Page Application (SPA) using Vue.js an
   - Use WhatsApp's API to facilitate direct messaging.
   - Button functionality that opens WhatsApp with a pre-filled message, ensuring ease of communication for all users.
 
-- **Accessibility Focus**
+- **Accessibility Focus**:
 - The application will adhere to WCAG (Web Content Accessibility Guidelines) standards to ensure usability for individuals with disabilities.
 - Features will include keyboard navigation, screen reader compatibility, and color contrast considerations to enhance user experience for everyone.
 
